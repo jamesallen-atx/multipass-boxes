4e8f8fe916520dcd6f521ca34daf9402e5befe1e
James Allen <10216753+jamesallen-atx@users.noreply.github.com>
2023-12-06 20:58:08 -0600

    add instance name arg

--- ubuntu-dev/init.sh
+++ ubuntu-dev/init.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+instance_name=$1
 if [[ $(uname -r) == *"microsoft"* ]];
 then
-    multipass.exe launch --name ubuntu-dev --cpus 6 --mem 16G --disk 160G --cloud-init ./cloud-config.yaml 20.04
-    multipass.exe set client.primary-name=ubuntu-dev
-    multipass.exe mount $HOME ubuntu-dev:HOME
-    multipass.exe mount $HOME ubuntu-dev:/home/james/HOME
+    multipass.exe launch --name $instance_name --cpus 6 --mem 16G --disk 160G --cloud-init ./cloud-config.yaml 22.04
+    multipass.exe set client.primary-name=$instance_name
+    multipass.exe mount $HOME $instance_name:HOME
+    multipass.exe mount $HOME $instance_name:/home/james/HOME
 else
-    multipass launch --name ubuntu-dev --cpus 6 --mem 16G --disk 160G --cloud-init ./cloud-config.yaml 20.04
-    multipass set client.primary-name=ubuntu-dev
-    multipass mount $HOME ubuntu-dev:HOME
-    multipass mount $HOME ubuntu-dev:/home/james/HOME
+    multipass launch --name $instance_name --cpus 6 --mem 16G --disk 160G --cloud-init ./cloud-config.yaml 22.04
+    multipass set client.primary-name=$instance_name
+    multipass mount $HOME $instance_name:HOME
+    multipass mount $HOME $instance_name:/home/james/HOME
 fi
