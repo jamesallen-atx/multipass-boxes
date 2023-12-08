3339510edb0451765377c05817c4d86e60b8e4a2
James Allen <10216753+jamesallen-atx@users.noreply.github.com>
2023-12-08 16:01:27 -0600

    update ansible test box with new key

--- ubuntu-ansible/init.sh
+++ ubuntu-ansible/init.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+instance_name=$1
 if [[ $(uname -r) == *"microsoft"* ]];
 then
-    multipass.exe launch --name ubuntu1 --cpus 2 --mem 2G --disk 15G --cloud-init ./cloud-config.yaml
+    multipass.exe launch --name $instance_name --cpus 6 --memory 16G --disk 160G --cloud-init ./cloud-config.yaml 22.04
 else
-    multipass launch --name ubuntu1 --cpus 2 --mem 2G --disk 15G --cloud-init ./cloud-config.yaml
+    multipass launch --name $instance_name --cpus 6 --memory 16G --disk 160G --cloud-init ./cloud-config.yaml 22.04
 fi
